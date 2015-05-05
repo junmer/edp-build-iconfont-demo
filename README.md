d484ba2624d36461831565c88017344e62e5025a
menglingjun <junmer@foxmail.com>
2015-05-05 20:29:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# edp-build iconfont
+
+> edp build iconfont from svgs
+
+## PreInstall
+
+```
+$ npm install -g edp
+```
+
+## Useage
+
+```
+$ git clone https://github.com/junmer/edp-build-iconfont-demo
+$ cd edp-build-iconfont-demo
+$ npm install
+$ edp build -f
+```
+
+## Config
+
+```
+var FontProcessor = require('edp-build-iconfont');
+var iconProcessor = new IconProcessor({
+    files: [ 'src/svg/*.svg' ],		// svg 文件
+    fontName: 'icon-food',			// 字体名
+    dest: 'asset/font'				// 目标路径
+});
+```
+
+## Showcase
+
+```
+$ edp webserver start
+```
+
+打开 `http://127.0.01:8848/output/` 看效果
+
+## Related
+
+- [edp](https://github.com/ecomfe/edp)
+- [fontmin](https://github.com/junmer/fontmin)
+- [edp-build-iconfont](https://github.com/junmer/edp-build-iconfont)
+- [fonteditor](https://github.com/ecomfe/fonteditor)
+
---
+++ edp-build-config.js
@@ -0,0 +1,69 @@
+exports.input = __dirname;
+
+var path = require( 'path' );
+exports.output = path.resolve( __dirname, 'output' );
+
+var IconProcessor = require('edp-build-iconfont');
+
+// var moduleEntries = 'html,htm,phtml,tpl,vm,js';
+// var pageEntries = 'html,htm,phtml,tpl,vm';
+
+exports.getProcessors = function () {
+    var lessProcessor = new LessCompiler();
+    var cssProcessor = new CssCompressor();
+    var moduleProcessor = new ModuleCompiler();
+    var jsProcessor = new JsCompressor();
+    var pathMapperProcessor = new PathMapper();
+    var addCopyright = new AddCopyright();
+    var iconProcessor = new IconProcessor({
+        files: [ 'src/svg/*.svg' ],
+        fontName: 'icon-food',
+        dest: 'asset/font'
+    });
+
+    return {
+        'default': [ lessProcessor, moduleProcessor, pathMapperProcessor, iconProcessor ],
+        'release': [
+            lessProcessor, cssProcessor, moduleProcessor,
+            jsProcessor, pathMapperProcessor, addCopyright, iconProcessor
+        ]
+    };
+};
+
+exports.exclude = [
+    'node_modules',
+    'screenshot',
+    'README.md',
+    'package.json',
+    'tool',
+    'doc',
+    'test',
+    'module.conf',
+    'dep/packages.manifest',
+    'dep/*/*/test',
+    'dep/*/*/doc',
+    'dep/*/*/demo',
+    'dep/*/*/tool',
+    'dep/*/*/*.md',
+    'dep/*/*/package.json',
+    'edp-*',
+    '.edpproj',
+    '.svn',
+    '.git',
+    '.gitignore',
+    '.idea',
+    '.project',
+    'Desktop.ini',
+    'Thumbs.db',
+    '.DS_Store',
+    '*.tmp',
+    '*.bak',
+    '*.swp'
+];
+
+exports.injectProcessor = function ( processors ) {
+    for ( var key in processors ) {
+        global[ key ] = processors[ key ];
+    }
+};
+
